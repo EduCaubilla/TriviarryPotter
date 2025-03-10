6e4dfa47d3f537342f135aa582ef41a9f0bac062
Edu Caubilla <educaubilla@gmail.com>
2025-03-10 10:41:57 +0100

    Initial Commit

---
+++ TriviarryPotter/TriviarryPotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviarryPotterApp.swift
+//  TriviarryPotter
+//
+//  Created by Edu Caubilla on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviarryPotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
